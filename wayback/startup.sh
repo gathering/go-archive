ca5285e6285d07d442aaa5224c21609e2f304337
Nicolai Rosdahl Tellefsen <niccofyren@gmail.com>
2023-10-01 17:23:11 +0200

    Fix git lfs support and add tg23 archives

--- wayback/startup.sh
+++ wayback/startup.sh
@@ -9,14 +9,19 @@ COLLECTIONS="$WORKDIR/collections"
 mkdir -p "$SOURCES"
 cd "$SOURCES"
 
+git lfs install
+
 echo "Cloning archive sources from git"
-git clone https://github.com/gathering/go-archive-tg21 || (cd go-archive-tg21 ; git pull ; cd ..)
-git clone https://github.com/gathering/go-archive-tg22 || (cd go-archive-tg22 ; git pull ; cd ..)
+git clone https://github.com/gathering/go-archive-tg21 || (cd go-archive-tg21 ; git pull ; git lfs pull ; cd ..)
+git clone https://github.com/gathering/go-archive-tg22 || (cd go-archive-tg22 ; git pull ; git lfs pull ; cd ..)
+git clone https://github.com/gathering/go-archive-tg23 || (cd go-archive-tg23 ; git pull ; git lfs pull ; cd ..)
 
 cd "$WORKDIR"
 
+# Copying files isn't very disk usage friendly, PR's for better strategies are welcome ;)
 echo "Populating collections from archive sources"
 cp -r "$SOURCES/go-archive-tg21/browsertrix-crawler/crawls/collections/tg21/" "$COLLECTIONS/"
 cp -r "$SOURCES/go-archive-tg22/browsertrix-crawler/tg22/" "$COLLECTIONS/"
+cp -r "$SOURCES/go-archive-tg23/tg23/" "$COLLECTIONS/"
 
 exec /docker-entrypoint.sh $@
