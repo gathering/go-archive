7603e8b941623a623798ec7362adc2fa9bdd1ac8
Nicolai Rosdahl Tellefsen <niccofyren@gmail.com>
2023-10-29 22:49:19 +0100

    Add "TG24" crawl and archive config

--- wayback/startup.sh
+++ wayback/startup.sh
@@ -15,6 +15,7 @@ echo "Cloning archive sources from git"
 git clone https://github.com/gathering/go-archive-tg21 || (cd go-archive-tg21 ; git pull ; git lfs pull ; cd ..)
 git clone https://github.com/gathering/go-archive-tg22 || (cd go-archive-tg22 ; git pull ; git lfs pull ; cd ..)
 git clone https://github.com/gathering/go-archive-tg23 || (cd go-archive-tg23 ; git pull ; git lfs pull ; cd ..)
+# git clone https://github.com/gathering/go-archive-tg24 || (cd go-archive-tg24 ; git pull ; git lfs pull ; cd ..)
 
 cd "$WORKDIR"
 
@@ -23,5 +24,6 @@ echo "Populating collections from archive sources"
 cp -r "$SOURCES/go-archive-tg21/browsertrix-crawler/crawls/collections/tg21/" "$COLLECTIONS/"
 cp -r "$SOURCES/go-archive-tg22/browsertrix-crawler/tg22/" "$COLLECTIONS/"
 cp -r "$SOURCES/go-archive-tg23/tg23/" "$COLLECTIONS/"
+#cp -r "$SOURCES/go-archive-tg24/tg24/" "$COLLECTIONS/"
 
 exec /docker-entrypoint.sh $@
